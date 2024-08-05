0278c9375266ae95d15a369d5479d681561a7348
AmrAlsyed <Xaxeth@gmail.com>
2024-08-05 19:08:46 +0300

    test3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,44 @@ const Footer = () => {
             <input /> <button>Subscribe</button>
           </div>
         </div>
-        <div className="f-btm"></div>
+        <hr className="bar" />
+        <div
+          className="css-1tefr08 f-btn"
+          style={{ justifyContent: "flex-start" }}
+        >
+          <div style={{ width: "10%" }}>
+            <span>
+              Lorem Ipsum is simply dummy text of the printing and typesett ing
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </span>
+          </div>
+          <ul className="nav-menu footer-menu">
+            <li>
+              <a href={"/"}>News</a>
+            </li>
+            <li>
+              <a href={"/project"}>Tournaments</a>
+            </li>
+            <li>
+              <a href={"/about"}>Courses</a>
+            </li>
+            <li>
+              <a href={"/contact"}>E-books</a>
+            </li>
+          </ul>
+          <ul className="nav-menu footer-menu">
+            <li>
+              <a href={"/"}>Privacy policies</a>
+            </li>
+            <li>
+              <a href={"/project"}>Terms & Conditions</a>
+            </li>
+            <li>
+              <a href={"/about"}>Contact Us</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
