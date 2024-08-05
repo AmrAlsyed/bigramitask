a6b019e5abdde8acf6730ac3babf23b15ce7b56a
AmrAlsyed <Xaxeth@gmail.com>
2024-08-05 18:53:03 +0300

    test2

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -7,6 +7,20 @@ const Book = () => {
     group2: ["Page 3 - 4"],
   });
   const [bookmark, setBookmark] = useState(["Page 3"]);
+  const [selectedPage, setSelectedPage] = useState("");
+  const [loadedPDF, setLoadedPDF] = useState("");
+
+  const handlePageClick = (page) => {
+    setSelectedPage(page);
+    if (page === "Page 1 - 2") {
+      setLoadedPDF("PDF1 is loaded here");
+    } else if (page === "Page 3 - 4") {
+      setLoadedPDF("PDF2 is loaded here");
+    } else {
+      setLoadedPDF("");
+    }
+  };
+
   return (
     <div>
       <div className="css-vw4wm5">
@@ -18,22 +32,43 @@ const Book = () => {
             className="css-58bxr1"
             style={{ justifyContent: "space-between" }}
           >
-            <div
-              className="css-waryrq"
-              style={{ background: "rgb(39, 39, 39)" }}
-            >
+            <div className="css-waryrq">
               <Accordion title="Pages">
                 <div>
                   <ul>
                     {pages.group1.map((page, index) => (
-                      <li key={index}>{page}</li>
+                      <li
+                        key={index}
+                        onClick={() => handlePageClick(page)}
+                        style={{
+                          backgroundColor:
+                            selectedPage === page
+                              ? "rgba(85, 231, 72, 0.5)"
+                              : "transparent",
+                          cursor: "pointer",
+                        }}
+                      >
+                        {page}
+                      </li>
                     ))}
                   </ul>
                 </div>
                 <div>
                   <ul>
                     {pages.group2.map((page, index) => (
-                      <li key={index}>{page}</li>
+                      <li
+                        key={index}
+                        onClick={() => handlePageClick(page)}
+                        style={{
+                          backgroundColor:
+                            selectedPage === page
+                              ? "rgba(85, 231, 72, 0.5)"
+                              : "transparent",
+                          cursor: "pointer",
+                        }}
+                      >
+                        {page}
+                      </li>
                     ))}
                   </ul>
                 </div>
@@ -41,14 +76,29 @@ const Book = () => {
               <Accordion title="Bookmark">
                 <ul>
                   {bookmark.map((page, index) => (
-                    <li key={index}>{page}</li>
+                    <li
+                      key={index}
+                      onClick={() => handlePageClick(page)}
+                      style={{
+                        backgroundColor:
+                          selectedPage === page
+                            ? "rgba(85, 231, 72, 0.5)"
+                            : "transparent",
+                        cursor: "pointer",
+                      }}
+                    >
+                      {page}
+                    </li>
                   ))}
                 </ul>
               </Accordion>
             </div>
             <div className="css-1ur3h3p">
               <div className="css-1aadjba"></div>
-              <div className="css-14or1vz"></div>
+              <div className="css-14or1vz">
+                <h1>{selectedPage}</h1>
+                <h1>{loadedPDF}</h1>
+              </div>
             </div>
           </div>
         </div>
